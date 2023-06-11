90e13f1ca97a9fee6ac63ed6c3e3186cf8158c55
rlacksgur <kch012904@naver.com>
2023-06-11 15:44:53 +0900

    massage

--- 22871.py
+++ 22871.py
@@ -10,3 +10,5 @@ def solve(limit, start): #start는 index
         for i in range(len(lst)):
             if i>start and (i-start)*(1+abs(lst[start]-lst[i]))<=limit:
                 stack.append()
+for i in range(n):
+    print("hello")
---
+++ web.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"}
+url = "https://www.acmicpc.net/source/61927657" 
+res = requests.get(url, headers=headers)
+res.raise_for_status() # 정상 200
+soup = BeautifulSoup(res.text, "lxml")
