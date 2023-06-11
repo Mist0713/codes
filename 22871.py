39715a1b70170cac7577dba7db5a6555135af729
rlacksgur <kch012904@naver.com>
2023-06-11 10:06:49 +0900

    wef

---
+++ 22871.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+lst = list(map(int, input().split()))
+
+def solve(limit, start): #start는 index
+    stack = [start]
+    while stack:
+        for i in range(len(lst)):
+            if i>start and (i-start)*(1+abs(lst[start]-lst[i]))<=limit:
+                stack.append()
