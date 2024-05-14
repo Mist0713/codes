399cc725035c37ce6e882780abf3185c5fc89cb7
Mist0713 <angmanymany@gmail.com>
2024-05-14 21:37:31 +0900

    2236

---
+++ 2236.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+lst = list(map(int, input().split()))
+
+lstForCompare = lst[:]
+lstForCompare.sort()
+
+for _ in range(-1, 0, -1):
+
+
+
+
+
