6b74fb0801ee2e67ad2ebef74f10ea13e90a5662
Mist0713 <angmanymany@gmail.com>
2024-05-16 23:17:42 +0900

    .2

--- 2236.py
+++ 2236.py
@@ -6,13 +6,31 @@ lst = list(map(int, input().split()))
 
 lstForCompare = lst[:]
 lstForCompare.sort()
+lstForCompare.reverse()
 
-print(lstForCompare)
+# print(lstForCompare)
 
 answerPower = []
-isPowerConnected = []
+isPowerConnected = [0] * N
 
-for i in range(K):
-    
-    answerPower.append(lst.index(lstForCompare[i]))
-    
+if N==K:
+    for i in range(K):
+        answerPower = [i for i in range(1, N+1)]
+        isPowerConnected = [i for i in range(1, N+1)]
+elif N>K:
+    for i in range(K):
+        answerPower.append(lst.index(lstForCompare[i])+1)
+        isPowerConnected[lst.index(lstForCompare[i])] = lst.index(lstForCompare[i])+1
+elif N<K:
+    for i in range(N):
+        answerPower.append(lst.index(lstForCompare[i])+1)
+        isPowerConnected[lst.index(lstForCompare[i])] = lst.index(lstForCompare[i])+1
+    for i in range(K-N):
+        answerPower.append(0)
+
+
+for i in answerPower:
+    print(i)
+
+for i in isPowerConnected:
+    print(i)
